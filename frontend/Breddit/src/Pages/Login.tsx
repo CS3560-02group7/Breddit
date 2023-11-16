5e0767cfaf96f0adef6eba4ecfcd9db8b5dd0d86
Darien Lin <darienlin87@gmail.com>
2023-11-16 00:44:56 -0800

    added background color and fixed login white space

--- frontend/Breddit/src/Pages/Login.tsx
+++ frontend/Breddit/src/Pages/Login.tsx
@@ -1,30 +1,33 @@
 import { Input } from 'antd';
 function LoginForm() {
     return (
-        <div style={{backgroundColor:"gray", padding:"0 0 20px 0", width:"50%"}} className="h-screen flex items-center justify-center flex-col">
-            <div>test</div>
-            <h1 className="text-3xl text-gray-900 dark:text-black" style={{textAlign:"center", padding:"20px"}}> Welcome to Breddit</h1>
+        <div className="h-screen flex">
+        <img src="https://tastesbetterfromscratch.com/wp-content/uploads/2020/03/Bread-Recipe-5-2.jpg"/>
+        <div className="h-screen flex items-center justify-center w-full bg-amber-700">
+        <div className="flex items-center justify-center flex-col h-3/5 w-2/5 bg-white rounded">
+            <h1 className="text-3xl text-gray-900 dark:text-black mb-8"> Welcome to Breddit</h1>
             <form action="" method="get" className="login-form" style={{
             display:"flex",
             alignItems: "center",
             justifyContent: "center",
             flexFlow: "column"}}>
-                <div className="login-form">
-                    <Input placeholder='Email:'>
+                <div className="login-form w-3/4">
+                    <Input placeholder='Email:'type="email" className="w-full mb-2">
                     </Input>
                 </div>
-                <div className="login-form">
-                    <Input placeholder='Password:' type="password">
+                <div className="login-form w-3/4">
+                    <Input placeholder='Password:' type="password" className="mb-5">
                     </Input>
                 </div>
-                <a href="login" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500">Forgot password?</a>
-                <br />
-                New to Breddit? <a href="signup" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500" >Sign up for an account.</a>
+                <a href="login" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500 mb-1">Forgot password?</a>
+                <p>New to Breddit? <a href="signup" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500 mb-1" >Sign up for an account.</a></p>
 
                 <div className="login-form w-full">
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Submit</button>
+                    <button type="submit" className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded w-full mt-4">Submit</button>
                 </div>
             </form>
+            </div>
+        </div>
         </div>
     );
 }
