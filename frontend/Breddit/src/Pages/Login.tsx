6accc7f5cdb7257fef6a22c6720ab152e807e614
richardpacheco00 <richard.a.pacheco.iii@gmail.com>
2023-11-15 21:05:11 -0800

    created login page omg

--- frontend/Breddit/src/Pages/Login.tsx
+++ frontend/Breddit/src/Pages/Login.tsx
@@ -1,10 +1,36 @@
+function LoginForm() {
+    return (
+        <>
+            <h1> Welcome to Breddit</h1>
+            <form action="" method="get" className="login-form">
+                <div className="login-form">
+                    <label htmlFor="name">Email: </label>
+                    <input type="email" name="email" id="loginEmail" required />
+                </div>
+                <div className="login-form">
+                    <label htmlFor="email">Password: </label>
+                    <input type="password" name="password" id="loginPassword" required />
+                </div>
+                <a href="login">Forgot password?</a>
+                <br />
+                New to Breddit? <a href="signup">Sign up for an account.</a>
+
+                <div className="login-form">
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+        </>
+    );
+}
+
 const Login = () => {
     return (
         <>
-        THIS IS THE LOGIn PAGE
+            <LoginForm />
         </>
 
     )
 }
+
 
 export default Login;
