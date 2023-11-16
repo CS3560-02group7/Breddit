b9863ba07a091819c9d5b63e12074b2bbb056594
Darien Lin <darienlin87@gmail.com>
2023-11-15 23:03:21 -0800

    added tailwind support and styled login page

--- frontend/Breddit/src/Pages/Login.tsx
+++ frontend/Breddit/src/Pages/Login.tsx
@@ -1,27 +1,36 @@
+import { Input } from 'antd';
 function LoginForm() {
     return (
-        <>
-            <h1> Welcome to Breddit</h1>
-            <form action="" method="get" className="login-form">
+        <div style={{backgroundColor:"gray", padding:"0 0 20px 0", width:"50%"}} className="h-screen flex items-center justify-center flex-col">
+            <div>test</div>
+            <h1 className="text-3xl text-gray-900 dark:text-black" style={{textAlign:"center", padding:"20px"}}> Welcome to Breddit</h1>
+            <form action="" method="get" className="login-form" style={{
+            display:"flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexFlow: "column"}}>
                 <div className="login-form">
-                    <label htmlFor="name">Email: </label>
-                    <input type="email" name="email" id="loginEmail" required />
+                    <Input placeholder='Email:'>
+                    </Input>
                 </div>
                 <div className="login-form">
-                    <label htmlFor="email">Password: </label>
-                    <input type="password" name="password" id="loginPassword" required />
+                    <Input placeholder='Password:' type="password">
+                    </Input>
                 </div>
-                <a href="login">Forgot password?</a>
+                <a href="login" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500">Forgot password?</a>
                 <br />
-                New to Breddit? <a href="signup">Sign up for an account.</a>
+                New to Breddit? <a href="signup" className="font-semibold text-blue-900 underline dark:text-blue decoration-blue-500" >Sign up for an account.</a>
 
-                <div className="login-form">
-                    <input type="submit" value="Login" />
+                <div className="login-form w-full">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Submit</button>
                 </div>
             </form>
-        </>
+        </div>
     );
 }
+
+
+
 
 const Login = () => {
     return (
