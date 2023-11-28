f4926ebbb1e10aa6aa63bcd90cb642ced12f0cbc
richardpacheco00 <richard.a.pacheco.iii@gmail.com>
2023-11-27 22:02:56 -0800

    god what am i doing with tailwind

--- frontend/Breddit/src/Pages/NotFound.tsx
+++ frontend/Breddit/src/Pages/NotFound.tsx
@@ -1,7 +1,15 @@
+
 const NotFound = () => {
     return (
         <>
-        <p>WE could not find that sadge :(</p>
+            <div className="flex flex-col bg-gray-800 h-screen place-content-center">
+                <div className="basis-1/3 center">
+                    Sorry, we couldn't find that :(
+                </div>
+                <button className="basis-1/3 text-white bg-amber-500 hover:bg-amber-600 w-1/6">
+                    Return to Home
+                </button>
+            </div>
         </>
     )
 }
