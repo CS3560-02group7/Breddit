22c4ac1a9b32027796ff5a616191e3f7cccd449d
richardpacheco00 <richard.a.pacheco.iii@gmail.com>
2023-11-27 23:00:51 -0800

    holy shit we are making progress

--- frontend/Breddit/src/Pages/NotFound.tsx
+++ frontend/Breddit/src/Pages/NotFound.tsx
@@ -1,14 +1,23 @@
-
 const NotFound = () => {
     return (
         <>
-            <div className="flex flex-col bg-gray-800 h-screen place-content-center">
-                <div className="basis-1/3 center">
-                    Sorry, we couldn't find that :(
+            <div className="flex bg-zinc-900 h-screen justify-center items-center">
+                <div className="flex flex-col bg-zinc-800 w-1/4 h-1/3 rounded-lg items-center">
+                    <div className="flex h-1/3 text-white items-center">
+                        <h1 className="text-2xl font-bold">Page Not Found</h1>
+                    </div>
+                    <div className="flex text-white h-1/3 items-center">
+                        Sorry, the page you requested does not exist.
+                    </div>
+                    <div className="flex items-center h-1/3">
+                        <a href="login">
+                            <button className="h-12 w-36 rounded-full bg-amber-500 hover:bg-amber-600 font-bold text-white">
+                                Return to Home
+                            </button>
+                        </a>
+                    </div>
+
                 </div>
-                <button className="basis-1/3 text-white bg-amber-500 hover:bg-amber-600 w-1/6">
-                    Return to Home
-                </button>
             </div>
         </>
     )
