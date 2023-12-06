5b6f25209d5364a0d2d26eae90729d3c0bf2b671
whatsGee <gadejesus09@gmail.com>
2023-12-06 04:22:18 -0800

    Update UserProfile.tsx

--- frontend/Breddit/src/Pages/UserProfile.tsx
+++ frontend/Breddit/src/Pages/UserProfile.tsx
@@ -44,54 +44,50 @@ const UserProfile = () => {
     if (userData){
         return(<div>
             <div className="canvas-paper">
-                <div className="h-screen bg-slate-300">
-            <div>
-                <header className="bg-slate-500 shadow-md justify-between items-center p-5 flex">
+              <div className="h-screen bg-gray-100">
+                <div>
+                  <header className="bg-amber-500 shadow-md justify-between items-center p-5 flex">
                     <nav>
-                        <a href="" className="text-2xl font-bold ml-5 text-offwhite">Breddit</a>
+                      <a href="" className="text-2xl font-bold ml-5 text-gray-800">Breddit</a>
                     </nav>
-                </header>
-            </div>
-            <div className="mx-auto container p-5 flex">
-                <div className="w-1/3">
-                    <div className="bg-offwhite shadow-md flex-col rounded p-5 flex space-y-5 relative">
-                        <img src="https://i.redd.it/1b08e2gy2xo81.jpg" className="self-center w-20 h-20 rounded-full"/>
-                            <div className="space-y-2">
-                            <p className="text-2xl font-bold text-gray-800">Username: {userData.username}</p>
-                                <div className="items-center justify-between flex">
-                                    <p className="text-sm text-gray-600">
-                                        Reputation: {userData.reputation}
-                                        <span className="text-sm text-gray-500">0</span>
-                                    </p>
-                                </div>
-                            </div>
-                                </div>
-                            </div>
-                            <div className="w-2/3 pl-5 grid grid-cols-1 gap-5">
-                                <section className="bg-offwhite shadow-md rounded p-5">
-                                <div className="justify-between items-center flex">
-                                    <p className="text-xl font-bold text-gray-800">Posts</p>
-                                    <></>
-                                    <button type="submit" className="hover:bg-green-700 rounded text-white bg-green-500
-                                        font-bold py-1 px-2" onClick = {goToCreatePost}>
-                                        <div className="fas fa-plus">+</div>
-                                    </button>
-                                    </div>
-                                </section>
-                                <section className="bg-offwhite shadow-md rounded p-5">
-                                <div className="justify-between items-center flex">
-                                    <p className="text-xl font-bold text-gray-800">Communities</p>
-                                    <button className="hover:bg-green-700 rounded text-white bg-green-500
-                                        font-bold py-1 px-2" onClick = {goToCreateCommunity}>
-                                    <div className="fas fa-plus"> + </div>
-                                    </button>
-                                </div>
-                                </section>
-                            </div>
-                        </div>
-                    </div>
+                  </header>
                 </div>
-            </div>)
+                <div className="mx-auto items-center container p-5 flex flex-col bg-gray-700 h-screen">
+                  <div className="bg-white shadow-md w-1/3 h-auto flex-col rounded flex space-y-5 p-5 relative">
+                    <img src="https://i.redd.it/1b08e2gy2xo81.jpg" className="self-center w-20 h-20 rounded-full"/>
+                    <div className="space-y-2">
+                      <p className="text-2xl font-bold text-gray-800 text-center">Username: {userData.username}</p>
+                      <p className="text-gray-500 text-center">Bio</p>
+                      <p className="text-gray-500 text-center">Reputation: {userData.reputation}</p>
+                    </div>
+                    <button type="submit" className="font-sans hover:bg-blue-700 rounded text-white bg-blue-500
+                        font-bold py-2 px-4">Edit Profile</button>
+                    <div className="absolute right-0 top-5"></div>
+                  </div>
+                  <div className="w-full mt-5 flex flex-row space-x-5">
+                    <section className="bg-white shadow-md w-2/3 rounded p-5">
+                      <div className="justify-between items-center flex">
+                        <p className="text-xl font-bold text-gray-800">Posts</p>
+                        <button type="submit" className="hover:bg-green-700 rounded text-white bg-green-500
+                            font-bold py-1 px-2" onClick={goToCreatePost}>
+                          <div className="fas fa-plus">+</div>
+                        </button>
+                      </div>
+                    </section>
+                    <section className="bg-white shadow-md w-1/2 rounded p-5">
+                      <div className="justify-between items-center flex">
+                        <p className="text-xl font-bold text-gray-800">Communities</p>
+                        <button type="submit" className="hover:bg-green-700 rounded text-white bg-green-500
+                            font-bold py-1 px-2" onClick={goToCreateCommunity}>
+                          <div className="fas fa-plus">+</div>
+                        </button>
+                      </div>
+                    </section>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>)
         
     }
     else{
