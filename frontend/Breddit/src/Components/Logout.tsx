6e4d5365fdd7727cb8328e685a7d352045328da2
whatsGee <gadejesus09@gmail.com>
2023-12-05 22:45:05 -0800

    Update Logout.tsx

--- frontend/Breddit/src/Components/Logout.tsx
+++ frontend/Breddit/src/Components/Logout.tsx
@@ -3,8 +3,8 @@ const Logout = () => {
   <div className="bg-white shadow-md text-center m-auto p-6 rounded">
     <p className="text-lg mb-4">Are you sure you want to log out?</p>
     <div className="justify-center flex">
-      <button type="submit" className="rounded bg-red-500 text-white py-2 px-4 mr-1.5">Yes</button>
-      <button type="submit" className="rounded bg-blue-500 text-white py-2 px-4 ml-1.5">No</button>
+      <button type="submit" className="rounded bg-gradient-to-r bg-amber-500 text-white py-2 px-4 hover:bg-amber-400 ease-in duration-200 mr-1.5">Yes</button>
+      <button type="submit" className="rounded bg-gradient-to-r bg-gray-700 text-white py-2 px-4 hover:bg-gray-600 ease-in duration-200 ml-1.5">No</button>
     </div>
   </div>
 </div>
